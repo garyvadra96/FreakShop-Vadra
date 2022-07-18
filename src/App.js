2b4bbc7f0a53f9e16b34cdd4388d417dadfb3e1c
garyvadra96 <agustinvadra@gmail.com>
2022-07-18 17:23:13 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-// import NavBar from './components/NavBar';
-// import ItemListContainer from './components/itemListContainer'; 
-// import Title from './components/Title';
-// import ClickTracker from './components/ClickTracker';
 import ItemCount from './components/componente entrega/ItemCount';
 
 function App() {
   return (<>
-  {/* <NavBar />
-  <ItemListContainer greeting="Bienvenidos"/> */}
-  {/* <Title /> */}
-  {/* <ClickTracker /> */}
-  <ItemCount stock="12" initial="0"/>
+  <ItemCount stock={12} initial={0} onAdd={(a, b) => console.log(a-b)} />
 
   </> 
   );
--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -3,27 +3,21 @@ import { useState, useEffect } from "react";
 const ItemCount = ({stock, initial, onAdd}) => {
     const [count, setCount] = useState(initial);
 
-    const registrarClick = (operacion) =>{
+
+    function registrarClick(operacion) {
         if (operacion === "-" && count > 0) {
-            setCount(count-1);
-        } else if (operacion === "+" && count < stock){
-            setCount(count+1);
+            setCount(count - 1);
+        } else if (operacion === "+" && count < stock) {
+            setCount(count + 1);
         }
     }
-
-    // const onAdd = () => {
-    //     if ((count > 0) && (stock < 12)) {
-    //         stock - count;
-    //         console.log(stock);
-    //     }
-    // }
 
     return ( <>
         <button onClick={() => registrarClick("-")}>-</button>
         {count}
         <button onClick={() => registrarClick("+")}>+</button>
         <h2>El stock disponible es: {stock}</h2>
-        <button onClick={() => onAdd(count)}>Add to Cart</button>
+        <button onClick={() => onAdd(stock, count)}>Add to Cart</button>
     </> );
 }
  
